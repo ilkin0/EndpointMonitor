fa8fe915b327b52277e5dc2d71141aa31841c6ab
Ilkin Mehdiyev <ilkin.mehidyev@logicomsoft.com>
2022-05-02 00:11:24 +0400

    init commit

---
+++ src/test/java/com/ilkinmehdiyev/endpointmonitor/EndpointMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilkinmehdiyev.endpointmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
